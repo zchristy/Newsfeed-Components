2b517e8480094283ff08e79e1281cd923b1ea646
zchristy <zchristy44@gmail.com>
2019-04-10 16:34:07 -0500

    Added Delete Article Button

--- Article/Article.js
+++ Article/Article.js
@@ -4,17 +4,31 @@ class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
+
     // create a reference to the ".expandButton" class.
     this.expandButton = this.domElement.querySelector('.expandButton');
+
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
+
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandArticle = this.expandArticle.bind(this)
     this.expandButton.addEventListener('click', this.expandArticle);
+
+    //==== Stretch Goal ====
+    this.readButton = this.domElement.querySelector('.readButton');
+    this.readButton.textContent = 'Done Reading - Delete';
+    this.deleteArticle = this.deleteArticle.bind(this);
+    this.readButton.addEventListener('click', this.deleteArticle);
   }
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+  }
+
+  //==== Stretch Goal ====
+  deleteArticle() {
+    this.domElement.classList.add('article-delete');
   }
 }
 
